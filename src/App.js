55153686b55437f796290b8ae45578a3c373c310
Jannaten Nayem <jannaten.nayem@eng.tamk.fi>
2021-01-09 17:06:16 +0200

    react query and query-devtools previous version added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 function App() {
   const [page, setPage] = useState("planets");
   return (
-    <div className="App">
-      <h1>Start Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === "planets" ? <Planets /> : <People />}
+    <div>
+      <div className="App">
+        <h1>Start Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
       </div>
+      <ReactQueryDevtools initialIsOpen={false} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "react-query";
+// import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>,
+  // <QueryClientProvider client={queryClient}>
+  <App />,
+  // </QueryClientProvider>,
   document.getElementById("root")
 );
